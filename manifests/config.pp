676b35728dc02fbe4ecb9f1e6b6062b4c073ba8b
Kevin Hrpcek <kevin.hrpcek@qg.com>
2015-03-31 14:09:05 -0500

    First version

---
+++ manifests/config.pp
@@ -0,0 +1,51 @@
+class grub::config inherits grub::params {
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        '5','6': {
+          augeas { 'grub.conf/password':
+            incl   => '/boot/grub/grub.conf',
+            lens   => 'grub.lns',
+            changes => [
+              "ins password after hiddenmenu",
+              "clear password/encrypted",
+              "set password $grub::params::md5_hash"
+            ],
+            onlyif  => "match password size == 0";
+          }
+        }
+        #'6': {
+        #  augeas { 'grub.conf/password':
+        #    context => "/files/boot/grub/grub.conf",            
+#incl   => '/boot/grub/grub.conf',
+            #lens   => 'grub.lns',
+         #   changes => [
+         #     "ins password after hiddenmenu",
+         #     "clear password/encrypted",
+         #     "set password \$1\$HQxCn0\$z4wH9dxlbbVLcSp1kjwuo0"
+          #  ],
+          #  onlyif  => "match password size == 0";
+          #}
+        #}
+        '7': {
+        #  do more stuff
+          file { '01_users':
+            path   => '/etc/grub.d/01_users',
+            ensure => 'file',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+            content => template('grub/01_users.erb')
+          }
+          exec { 'grub2-mkconfig':
+            path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin',
+            command => 'grub2-mkconfig -o /boot/grub2/grub.cfg',
+            subscribe => File['/etc/grub.d/01_users'],
+            refreshonly => 'true',
+          }
+            
+        }
+      }
+    }
+  }
+}
